15761bd42750852399b1b566f5916f5330771405
Panya <panyakor@gmail.com>
2011-04-26 03:51:03 +0400

    fixed path in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
 for i in ~/.vim ~/.vimrc ~/.gvimrc; do [ -e $i ] && mv $i $i.old; done
-git clone git://github.com/panya/janus.git ~/.vim
+git clone git://github.com/Panya/janus.git ~/.vim
 cd ~/.vim
 rake
