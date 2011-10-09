15f850eaae5817bd2b5bc763f09797ba20d2b829
Mikhail Korepanov <panyakor@gmail.com>
2011-10-09 22:31:27 +0400

    in master branch

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ VIM_DIR=~/.vim
 for i in $VIM_DIR ~/.vimrc ~/.gvimrc; do [ -e $i ] && mv $i $i.old; done
 
 # Clone my vimrc
-git clone -b pathogen git://github.com/Panya/dotvim.git $VIM_DIR
+git clone git://github.com/Panya/dotvim.git $VIM_DIR
 
 # Init Pathogen
 mkdir -p $VIM_DIR/autoload; curl -so $VIM_DIR/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/HEAD/autoload/pathogen.vim
