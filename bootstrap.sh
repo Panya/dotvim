fe9d40208aceff2d134a248608852012c7ba7794
Mikhail Korepanov <panyakor@gmail.com>
2018-06-30 15:47:02 +0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,5 @@
 VIM_DIR=~/.vim
+NVIM_DIR=~/.config/nvim
 
 # Backup old .vim
 for i in $VIM_DIR ~/.vimrc ~/.gvimrc; do [ -e $i ] && mv $i $i.old; done
@@ -7,11 +8,13 @@ for i in $VIM_DIR ~/.vimrc ~/.gvimrc; do [ -e $i ] && mv $i $i.old; done
 git clone git://github.com/Panya/dotvim.git $VIM_DIR
 cd $VIM_DIR
 
-# Update Pathogen bundle
-git submodule init; git submodule update
-
 # Add links to vimrc and gvimrc
 for i in vimrc gvimrc; do ln -s $VIM_DIR/$i ~/.$i; done
 
 # Cleanup
 git clean -fd
+
+# Add nvim config
+if [ ! -f "$NVIM_DIR/init.vim" ]; then
+  printf "set runtimepath^=$VIM_DIR runtimepath+=$VIM_DIR/after\nlet &packpath = &runtimepath\nsource ~/.vimrc" > $NVIM_DIR/init.vim
+fi
